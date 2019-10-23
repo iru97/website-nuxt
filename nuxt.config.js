de0f339fbb9745085ce50cd956d9c1dba999cb61
Iru Hernández Sánchez <iru.hernandez@ultebra.eu>
2019-10-23 08:09:55 +0100

    added font

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,6 +20,11 @@ export default {
                 rel: 'stylesheet',
                 href:
           'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
+            },
+            {
+                rel: 'stylesheet',
+                href:
+          'https://fonts.googleapis.com/css?family=Abel&display=swap'
             }
         ]
     },
